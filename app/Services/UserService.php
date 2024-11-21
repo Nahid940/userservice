0b5c45e288544e855f78ef7346237c1a221e5ded
nahid940 <nahid940@gmail.com>
2024-11-21 23:22:37 +0600

    New Changes added

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -11,8 +11,19 @@ class UserService {
     public function getUsers()
     {
         try{
-            $users = User::select('name', 'email' ,'phone', 'age', 'created_at')->paginate(20);
-            return UserLimitedDataResource::collection($users);
+            $users =  User::select('name', 'email' ,'phone', 'age', 'created_at')->paginate(10);
+            $data = UserLimitedDataResource::collection($users);
+            return [
+                'data' => $data, // Send the paginated data
+                'meta' => [
+                    'total' => $users->total(),   // Total records
+                    'per_page' => $users->perPage(), // Items per page
+                    'current_page' => $users->currentPage(), // Current page
+                    'last_page' => $users->lastPage(), // Last page
+                    'next_page_url' => $users->nextPageUrl(),
+                    'prev_page_url' => $users->previousPageUrl(),
+                ],
+            ];
         }catch(Exception $exp)
         {
             Log::info($exp->getMessage());
